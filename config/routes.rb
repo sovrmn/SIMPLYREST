8890482e63cf60af8e75eb0a934113b0e2025aad
Eliot Nemoz <nemoz.eliot@gmail.com>
2020-02-27 15:27:50 +0100

    modal sur les orders items

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :show, :new, :create]
   end
   resources :orders, only: [:update]
+  resources :order_items, only: [:update]
 
 
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
