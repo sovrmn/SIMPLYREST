33e0bffea84dbca39b9fa8fec1c454d51fd35436
Sovana Romano <sovana_srinn@hotmail.com>
2020-02-28 12:58:05 +0100

    debug heroku

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :supplier_item
   belongs_to :order
-  has_many :products, through: :supplier_item
+  has_many :products, through: :supplier_items
 
 end
