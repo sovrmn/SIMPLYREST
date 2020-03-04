193f3a9f39f01afc2ead709e0450af76f78d088b
Eliot Nemoz <nemoz.eliot@gmail.com>
2020-03-04 13:45:17 +0100

    landing page

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module SIMPLYREST
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.to_prepare do
+      # Configure single controller layout
+      Devise::SessionsController.layout "devise"
+    end
+
   end
 end
