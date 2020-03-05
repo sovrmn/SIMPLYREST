7a6867d3eac70accaec0238c1b4a6cebe221fa5a
Leo Guezi <leo.guezi@protonmail.com>
2020-03-05 16:32:57 +0100

    meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def default_url_options
+    { host: ENV["https://www.simplyrest.org/"] || "localhost:3000" }
+  end
+
 end
